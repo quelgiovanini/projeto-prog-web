443c87a3625f9fbad0ad9bdf46eb5f520fd68c04
Fellipe de Souza <>
2013-01-17 09:30:16 -0200

    trataCadastroAtividade

---
+++ src/java/control/trataCadastroAtividade.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import dao.AtividadeDAO;
+import dao.TipoAtividadeDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+import model.Atividade;
+import model.TipoAtividade;
+import model.Usuario;
+
+/**
+ *
+ * @author Quele
+ */
+
+public class trataCadastroAtividade extends Comando {
+
+    
+    @Override
+    public void execute() throws ServletException, IOException, SQLException, ClassNotFoundException {
+        getResponse().setContentType("text/html;charset=UTF-8");
+        PrintWriter out = getResponse().getWriter();
+        try {
+            getResponse().setContentType("text/html");
+            
+            try {
+            List tipoAtividades = new TipoAtividadeDAO().pesquisarTipo();
+           // TipoAtividade tipoAtiv;
+        //    tipoAtiv = new TipoAtividade(1,"Teste");
+            getRequest().setAttribute("tipos", tipoAtividades);
+            RequestDispatcher dispatcher = getRequest().getRequestDispatcher("cadastraAtividade.jsp");
+            if (dispatcher != null){
+                dispatcher.forward(getRequest(), getResponse());
+            }
+            }catch(SQLException ex1){
+            throw new ServletException(ex1);
+            }
+            
+            Integer codAtiv    = Integer.parseInt(getRequest().getParameter("codAtiv"));            
+            int aluno  = Integer.parseInt(getRequest().getParameter("aluno"));
+            int tipoAtividade     = Integer.parseInt(getRequest().getParameter("tipoAtividade"));
+            int professor   = Integer.parseInt(getRequest().getParameter("professor"));
+            String status = getRequest().getParameter("status");
+        
+            HttpSession session = getRequest().getSession(false);        
+            Usuario value = (Usuario) session.getAttribute("usuario");
+        
+            Atividade ativ = new Atividade(aluno, tipoAtividade, professor, status);
+            new AtividadeDAO().inserir(ativ);
+            getResponse().sendRedirect("index.jsp");
+        
+        } catch(SQLException ex){
+            throw new ServletException(ex);
+        }
+    }
+}
